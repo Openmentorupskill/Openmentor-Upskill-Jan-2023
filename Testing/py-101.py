34a8a551e97a700e6ce21ba9167e9cb8e178ee22
Openmentorupskill <124124570+Openmentorupskill@users.noreply.github.com>
2023-03-09 15:16:58 +0530

    py-101 file uploaded

---
+++ Testing/py-101.py
@@ -0,0 +1,19 @@
+#this is a comment
+# this is another comment
+
+print("hello all - welcome to upskill program")
+print('this is also allowed')
+
+a = input("Give your name:")
+print(a)
+
+b = int(input("Give your age:"))
+print(b)
+
+if (b >= 65):
+    print("you are a senior citizen")
+else:
+    print("you are young fellow")
+
+
+
